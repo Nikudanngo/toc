055b50ae57c0159c62be4c49f84ccde65164e4b3
Nikudanngo <satoru_rutosa@icloud.com>
2024-05-13 18:40:27 +0900

    fix complete page

--- src/app/complete/page.tsx
+++ src/app/complete/page.tsx
@@ -1,16 +1,3 @@
-"use client";
-import { Suspense } from "react";
-import { auth } from "@/config/Firebase";
-
-const Email = () => {
-  const user = auth.currentUser;
-  return (
-    <p className="text-center font-bold bg-gray-900/20 rounded-md p-2">
-      {user?.email}
-    </p>
-  );
-};
-
 export default function Complete() {
   // const payment_intent = searchParams.get("payment_intent");
   // const payment_intent_client_secret = searchParams.get(
@@ -19,14 +6,19 @@ export default function Complete() {
   // const redirect_status = searchParams.get("redirect_status");
 
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className="m-4">
-        <h1 className="text-2xl">決済が完了しました。</h1>
-        <p>以下のメールアドレスへ領収書をお送りしましたのでご確認ください。</p>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Email />
-        </Suspense>
-      </div>
+    <div className="max-w-7xl mx-auto m-4 text-center">
+      <h1 className="text-2xl mb-4 font-bold">決済が完了しました。</h1>
+      <p className="text-xl mb-2">ご購入いただきありがとうございました。</p>
+      <p className="text-base mb-2">
+        登録いただいたメールアドレスへ領収書をお送りしましたのでご確認ください。
+      </p>
+      <p className="mb-2">
+        お問い合わせは
+        <span>
+          <button className="text-blue-600 underline">連絡先</button>
+        </span>
+        にて記載しておりますので、お気軽にお問い合わせください。
+      </p>
     </div>
   );
 }
